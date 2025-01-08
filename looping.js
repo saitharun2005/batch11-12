98462eecf6c5ee8660d9e2e664c4a9573d5dec85
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-08 10:29:36 +0530

    saitharun

---
+++ looping.js
@@ -0,0 +1,22 @@
+// for(let a=0;a<100;a++){
+//     if(a%2==0){
+//         console.log("Even number = "+a);
+//     }else{
+//         console.log("Odd number = "+a);
+//     }
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log("Good Morning!");
+//     i++
+// }
+// let j=1;
+// let n=5;
+// do{
+//     j++;
+//     console.log("My name is saitharun");
+// }while(j<=n);
+function addtocart(){
+    alert("Product is added to cart");
+}
