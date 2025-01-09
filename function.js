9a04f9185db122de7c9eca27f46b0714575e4254
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-09 11:49:06 +0530

    saitharun

---
+++ function.js
@@ -0,0 +1,52 @@
+// !function with parameters
+
+// function userDetails(name , age , city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("saitharun",20,"Hyderabad")
+
+// anonymous function
+// function(){
+//     console.log("Anonymus");
+// }
+// ();
+
+
+
+// function expression
+// x=function(){
+//     console.log("function expression");
+// };
+// x();
+
+// IIFE
+// (function(){
+//     console.log("IIFE");
+// })();
+
+// arrow function
+// function demo   ()
+// {
+//     console.log("Hello");
+// }
+// demo();
+
+// let x= ()=>console.log("Arrow Function")
+// x();
+
+
+// let x = _ =>console.log("Arrow function");
+// x();
+
+// let x=a=>console.log(a);
+// x(5);
+
+// let x=(a,b)=>console.log(a+b);
+// x(5,5);
+
+// let x=_=>{console.log("Hi");
+//         console.log("bye");
+//         console.log("Hello")}
+// x();
