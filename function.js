495b76c5f9137e9d20fa935113a81263eca32fa1
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-10 10:36:39 +0530

    saitharun

--- function.js
+++ function.js
@@ -50,3 +50,55 @@
 //         console.log("bye");
 //         console.log("Hello")}
 // x();
+
+// return keyboard
+// function demo(a,b){
+//     console.log("hi");
+//     console.log("saitharun");
+//     return a+b;
+// };
+// let x=demo(5,5);
+// console.log(x);
+
+
+// let y =(a,b)=>a+b; //implicit return
+// let a =y(10,20);
+// console.log(a);
+
+
+// let z=(a,b)=>{return a+b}; //explicit return 
+// let b=z(10,120);
+// console.log(b);
+
+//Higher order function
+
+// function hof(a){
+//     return a;
+// };
+// let a= hof(function(){return "Hello I am Callback function"})
+// console.log(a());
+
+// function parent(callback){//higher order function
+//     return callback;
+// }
+// let x=parent(add(5,5));
+// console.log(x);
+
+// function add(a,b){//callback function
+//     return a+b
+// }
+
+//closure
+var a=100;
+let b="hi";
+function x(){
+    var user="saitharun";
+    let company="google";
+    const sal=123456789;
+    console.log(user);
+    console.log(company);
+    console.log(sal);
+    console.log(a);
+    console.log(b);
+};
+x();
---
+++ program.js
@@ -0,0 +1,32 @@
+// //square pattern
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=5;j++){
+//         document.writeln("*")
+//     }
+//     document.writeln("<br>")
+// }
+
+
+//right angle triangle pattern
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=5;j++){
+//         if(i>=j){
+//             document.writeln("*")
+//         }else{
+//             document.writeln("&nbsp;")
+//         }
+//     }
+//     document.writeln("<br>")
+// }
+
+// x pattern
+for(let i=1;i<=5;i++){
+     for(let j=1;j<=5;j++){
+        if(i==j || i+j==6){
+             document.writeln("*")
+        }else{
+            document.writeln("&nbsp;&nbsp;&nbsp;")
+        }
+    }
+    document.writeln("<br>")
+}
