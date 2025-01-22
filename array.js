8e47b51cce7d1aea4684284d7d954f358cc782e2
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-22 12:59:04 +0530

    saitharun

--- array.js
+++ array.js
@@ -1,56 +1,140 @@
-// const user1="saitharun";
-// const user2="Nithish";
-// const user3="Vamshi";
-// const user4="Raju";
-// const user5="Raghu";
+// let user1 = "Ramya";
+// let user2 = "SaiSmitha";
+// let user3 = "Sai nikhil";
+// let user4 = "Rahul";
+// let user5 = "Prabhas";
+
 // console.log(user1);
 // console.log(user2);
 // console.log(user3);
 // console.log(user4);
 // console.log(user5);
 
+// let users = ["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+// console.log(users[0]);
+// console.log(users[4]);
+// for(let i = 0 ; i<users.length; i++)
+// {
+//     console.log(users[i]);
+// }
 
-// const users =["saitharun","nithish","vamshi","raju","raghu"];
-// // console.log(users[0]);
-// let x=users.map((user)=>{
-//     return user;
+
+//let users = ["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+// let x = users.map((user)=>{
+//     // console.log(user);
+//     return user; //!["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
 // });
 // console.log(x);
 
-const users =["saitharun","nithish","vamshi","raju","raghu"];
-// users.pop();
-// console.log(users);
+// let x = users.forEach((user)=>{
+//     // console.log(user);
+//     return user;
+// });
+// console.log(x); // !undefined
 
-// users.unshift("gita");
-// console.log(users);
 
-// users.shift();
-// console.log(users);
+//! create array of objects and print the values.
 
-// console.log(users.reverse());
-
-// let x= users.join("")
-// console.log(x,typeof(x));
-
-// let frontend=["html","css","Js","REACTJS","ANGULAR",];
-// let backend=["nodejs","java","php","Golang"];
-// let db=["Mongodb","MYSQL","Oracle","MariaDB"];
-// let fullstack =frontend.concat(backend);
-// let project=fullstack.concat(db);
-// console.log(project);
-
-//ojects
-
-// let userdetails={
-//     name:"saitharun",
-//     age:20,
-//     city:"Hyderabad",
-//     address:{
-//         area:"Ramalayam",
-//         streetno:"1",
-//         pincode:"500048"
+// let userDetails = {
+//     name : "Pranav",
+//     age:22,
+//     company :"Amazon",
+//     sal : 1234567890,
+//     details:function(){
+//         return `My name is ${this.name} and i am working in ${this.company}`
 //     }
+// };
+// console.log(userDetails);
+// console.log(userDetails.name);
+// console.log(userDetails.details());
+
+//! create nested object and access the property of nested object
+
+// let userDetails = {
+//     name : "Pranav",
+//     age:22,
+//     company :"Amazon",
+//     sal : 1234567890,
+//     address:{
+//         city:"Hyd",
+//         area :{
+//             areaName:"Hitech",
+//             pincode :123456
+//         }
+//     }
+
 // }
-// console.log(userdetails.address.area);
 
+// console.log(userDetails.address.city);
+// console.log(userDetails.address.area.areaName);
 
+//! create array of objects and print the values.
+
+// let usersDetails = [
+//     {
+//         name:"Ramya",
+//         city:"Guntur"
+//     },
+//     {
+//         name:"Sai Nikhil",
+//         city:"Karimnagar"
+//     },
+//     {
+//         name:"Rahul",
+//         city:"Ananthpur"
+//     },
+//     {
+//         name:"Vaishali",
+//         city:"Nizambad"
+//     },
+//     {
+//         name:"Priya",
+//         city:"Kadapa"
+//     }
+// ];
+
+// usersDetails.map((x)=>{
+//     console.log(x.name);
+// })
+
+// console.log(usersDetails[3].name);
+
+//! JSON 
+
+// let userDetails ={
+//     name:"Sam",
+//     age:25,
+//     city:"Hyd"
+// };
+// console.log(userDetails);
+
+// let x = JSON.stringify(userDetails);
+// console.log("JSON object "+x);
+
+// let  y = JSON.parse(x);
+// console.log(y);
+function fetchUsers()
+{
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    response.then((result)=>{
+       return result.json().then((data)=>{
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map((user)=>{
+                store.innerHTML +=`
+                <tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.company.name}</td>
+                </tr>`
+
+            })
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+fetchUsers();
