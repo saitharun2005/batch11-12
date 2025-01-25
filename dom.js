b16d5072a038e79f8ec4f006807e7dc4923de1d8
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-25 13:01:34 +0530

    saitharun

--- dom.js
+++ dom.js
@@ -53,13 +53,51 @@
 //     console.log('Button Double clicked');
 // })
 
-let divBlock = document.getElementById('divBlock');
-divBlock.addEventListener('onMouseOver', () => {
-    document.body.style.backgroundColor = 'yellow';
-    divBlock.style.backgroundColor = 'blue';
-})
+// let divBlock = document.getElementById('divBlock');
+// divBlock.addEventListener('onMouseOver', () => {
+//     document.body.style.backgroundColor = 'yellow';
+//     divBlock.style.backgroundColor = 'blue';
+// })
 
-divBlock.addEventListener('onMouseLeave', () => {
-    document.body.style.backgroundColor = 'yellow';
-    divBlock.style.backgroundColor = 'blue';
+// divBlock.addEventListener('onMouseLeave', () => {
+//     document.body.style.backgroundColor = 'yellow';
+//     divBlock.style.backgroundColor = 'blue';
+// })
+
+// let ele = document.createElement("h1");
+// ele.innerText="My name is nithish";
+// ele.id="demo";
+// console.log(ele);
+
+// let ele= document.createElement("h1");
+// ele.innertext="My name is nithish";
+// ele.id="demo";
+// console.log(ele);
+
+// let image= document.createElement("img");   
+// image.src="./776477ceb80263933b6e6576d87bc9b4.jpg";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+// // document.body.appendChild(ele);
+
+let form = document.querySelector("form");
+let username = document.getElementById('uName');
+let mail = document.getElementById("uMail");
+let psw =  document.getElementById("uPass");
+
+
+
+form. addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let uname = username.value;
+    let uemail = mail.value;
+    let upass = psw.value;
+    let userDetails={
+        uname, uemail, upass
+    }
+    console.log(userDetails);
+    localStorage.setItem("userData", JSON.stringify(userDetails))
+
 })
