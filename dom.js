b9f5a36b52f205ef3bc7c14ec6719d6cdc5790a3
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-01-27 13:06:13 +0530

    saitharun

--- dom.js
+++ dom.js
@@ -1,103 +1,181 @@
-// let element = document.getElementById("demo");
-// element.innerText="Google+";
+// // // let element = document.getElementById("demo");
+// // // element.innerText="Google+";
 
-// console.log(element);
+// // // console.log(element);
 
-// let ele = document.getElementsByClassName("test");
-// console.log(ele);
-// console.log(Array.isArray(ele));
-// ele[0].innerText="Hello";
-// ele[0].style.backgroundColor="red";
+// // // let ele = document.getElementsByClassName("test");
+// // // console.log(ele);
+// // // console.log(Array.isArray(ele));
+// // // ele[0].innerText="Hello";
+// // // ele[0].style.backgroundColor="red";
 
-// let x=[...ele];
-// console.log(x,Array.isArray(x));
+// // // let x=[...ele];
+// // // console.log(x,Array.isArray(x));
 
-// x.map(element=>{
-//     element.innerText="Hello";
-//     element.style.backgroundColor="tomato";
-// })
+// // // x.map(element=>{
+// // //     element.innerText="Hello";
+// // //     element.style.backgroundColor="tomato";
+// // // })
 
-// let ele = document.getElementsByTagName("div");
-// console.log(ele);
+// // // let ele = document.getElementsByTagName("div");
+// // // console.log(ele);
 
-// let ele = document.getElementsByName("formData");
-// [...ele].map(inp=>{
-//     console.log(inp.value);
-// })
+// // // let ele = document.getElementsByName("formData");
+// // // [...ele].map(inp=>{
+// // //     console.log(inp.value);
+// // // })
 
-// let ele = document.getElementById("mru");
-// console.log(ele);
-
-
-
-// let ele = document.querySelector("#mru");
-// console.log(ele);
-
-// let ele = document.querySelector(".test");
-// console.log(ele);
+// // // let ele = document.getElementById("mru");
+// // // console.log(ele);
 
 
-// let ele = document.querySelectorAll(".test");
-// console.log(ele);
+
+// // // let ele = document.querySelector("#mru");
+// // // console.log(ele);
+
+// // // let ele = document.querySelector(".test");
+// // // console.log(ele);
 
 
-// let btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-//     console.log('Button clicked');
-//     alert('Button clicked');
-//     document.body.style.backgroundColor = 'red';
-// })
+// // // let ele = document.querySelectorAll(".test");
+// // // console.log(ele);
 
-// let btn = document.querySelector('#btn');
-// btn.addEventListener('dblclick', () => {
-//     console.log('Button Double clicked');
-// })
 
-// let divBlock = document.getElementById('divBlock');
-// divBlock.addEventListener('onMouseOver', () => {
-//     document.body.style.backgroundColor = 'yellow';
-//     divBlock.style.backgroundColor = 'blue';
-// })
+// // // let btn = document.querySelector('#btn');
+// // // btn.addEventListener('click', () => {
+// // //     console.log('Button clicked');
+// // //     alert('Button clicked');
+// // //     document.body.style.backgroundColor = 'red';
+// // // })
 
-// divBlock.addEventListener('onMouseLeave', () => {
-//     document.body.style.backgroundColor = 'yellow';
-//     divBlock.style.backgroundColor = 'blue';
-// })
+// // // let btn = document.querySelector('#btn');
+// // // btn.addEventListener('dblclick', () => {
+// // //     console.log('Button Double clicked');
+// // // })
 
-// let ele = document.createElement("h1");
-// ele.innerText="My name is nithish";
-// ele.id="demo";
-// console.log(ele);
+// // // let divBlock = document.getElementById('divBlock');
+// // // divBlock.addEventListener('onMouseOver', () => {
+// // //     document.body.style.backgroundColor = 'yellow';
+// // //     divBlock.style.backgroundColor = 'blue';
+// // // })
 
-// let ele= document.createElement("h1");
-// ele.innertext="My name is nithish";
-// ele.id="demo";
-// console.log(ele);
+// // // divBlock.addEventListener('onMouseLeave', () => {
+// // //     document.body.style.backgroundColor = 'yellow';
+// // //     divBlock.style.backgroundColor = 'blue';
+// // // })
 
-// let image= document.createElement("img");   
+// // // let ele = document.createElement("h1");
+// // // ele.innerText="My name is nithish";
+// // // ele.id="demo";
+// // // console.log(ele);
+
+// // // let ele= document.createElement("h1");
+// // // ele.innertext="My name is nithish";
+// // // ele.id="demo";
+// // // console.log(ele);
+
+// // // let image= document.createElement("img");   
+// // // image.src="./776477ceb80263933b6e6576d87bc9b4.jpg";
+// // // console.log(image);
+
+// // // document.body.appendChild(ele);
+// // // document.body.appendChild(image);
+// // // // document.body.appendChild(ele);
+
+// // let form = document.querySelector("form");
+// // let username = document.getElementById('uName');
+// // let mail = document.getElementById("uMail");
+// // let psw =  document.getElementById("uPass");
+
+
+
+// // form. addEventListener("submit",(event)=>{
+// //     event.preventDefault();
+// //     let uname = username.value;
+// //     let uemail = mail.value;
+// //     let upass = psw.value;
+// //     let userDetails={
+// //         uname, uemail, upass
+// //     }
+// //     console.log(userDetails);
+// //     localStorage.setItem("userData", JSON.stringify(userDetails))
+
+// // })
+
+// // let vd = document.createElement("video");
+// // vd.setAttribute("src","./video.mp4");
+// // vd.setAttribute("controls","controls");
+// // console.log(vd);
+
+// // document.body.appendChild(vd);
+
+
+
+// // let h1= document.createElement("h1");
+// // h1.setAttribute("id","demo");
+// // h1.innertext="My name is nithish";
+// // console.log(h1);
+
+// // document.body.appendChild(h1);
+
+// // let vd=document.createElement("video");
+// //!
+// let mainEle=document.createElement("div");
+// mainEle.setAttribute("id","mainBlock");
+// mainEle.style.border="2px solid red";
+// mainEle.style.width="550px";
+// mainEle.style.height="550px";
+
+
+// console.log(mainEle);
+
+
+// let topEle=document.createElement("div");
+// topEle.setAttribute("class","topblock");
+// // console.log(topEle);
+
+// let image = document.createElement("img");
 // image.src="./776477ceb80263933b6e6576d87bc9b4.jpg";
-// console.log(image);
+// image.style.width="550px";
 
-// document.body.appendChild(ele);
-// document.body.appendChild(image);
-// // document.body.appendChild(ele);
+
+// let botEle=document.createElement("div");
+// botEle.setAttribute("class","bottomBlock");
+// // console.log(botEle);
+
+// let h1=document.createElement("h1");
+// h1.innerText="Rohith Sharma 45";
+// h1.style.textAlign="center";
+
+// let btn = document.createElement("button");
+// btn.innerText="Click me";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.backgroundColor="blue";
+// btn.style.color="white";
+
+// botEle.appendChild(h1);
+// botEle.appendChild(btn);
+
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(botEle);
+// document.body.appendChild(mainEle);
+
 
 let form = document.querySelector("form");
+
 let username = document.getElementById('uName');
-let mail = document.getElementById("uMail");
-let psw =  document.getElementById("uPass");
+let password =  document.getElementById("uPass");
 
+let gender= document.getElementsByName("gender");
 
-
-form. addEventListener("submit",(event)=>{
+form.addEventListener("submit",(event)=>{
     event.preventDefault();
     let uname = username.value;
-    let uemail = mail.value;
-    let upass = psw.value;
-    let userDetails={
-        uname, uemail, upass
-    }
-    console.log(userDetails);
-    localStorage.setItem("userData", JSON.stringify(userDetails))
+    let upass = password.value;
+    let ugen = gender.value;
+
+    console.log(uname,upass,ugen);
 
 })
